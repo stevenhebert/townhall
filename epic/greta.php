74c62b96527f0d79f68fe004b78dca70a01f26e4
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-30 13:03:00 -0600

    Created file for persona named Greta

---
+++ epic/greta.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Persona Named Greta</title>
+	</head>
+	<body>
+		<h1>Person named Greta</h1>
+		<p>This persona named Greta is a constituent and a potential user of the town hall.  Her main issue is that sh'es disabled and cannot attend regularly scheduled town halls because she's pretty much housebound.  She is very interested in politics but finds it hard to participate in the process because of her disability.  She is very interested in social services as well as services for the disabled.</p>
+
+	</body>
+</html>
