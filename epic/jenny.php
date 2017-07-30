a078c9984ae2921df205373c4fbcd8f9b263bbe8
JRyanHenson <hensojr@gmail.com>
2017-07-30 16:43:50 -0600

    Added persona for user Jenny.

---
+++ epic/jenny.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Jenny's Persona</title>
+	</head>
+	<body>
+		<h2>Persona</h2>
+		<p><strong>Name:</strong>Jenny Jenkins<p>
+		<p><strong>Age:</strong> 24</p>
+		<p><strong>Technology:</strong>Mac and IPhone power user. Jenny uses a Mac Book Pro, and an IPhone 7 with an unlimited data plan. She uses Chrome browser because of it's cross-platform support and ease of use.</p>
+		<p><strong>Attitudes and Behaviors:</strong>Jenny is a college graduate and intellectually curious. She works in marketing and has a new found personal interest in politics and activism. She is actively looking for ways she can become more involved in local politics and connect with like minded individuals.</p>
+		<p><strong>Frustrations and Needs:</strong>Jenny is frustrated by how hard it to find information about city politics and link up with others for civil dialog. She needs a user friendly site that provides the information regarding city politics and a platform to discuss issues with her peers and maybe communicate directly with city politicians.</p>
+		<p><strong>Goals:</strong>Jenny wants to find a trustworthy site that clearly conveys the information about the city government she needs. She is comfortable with moderately complex websites that clearly state the relevant information.</p>
+		<p><strong>Story:</strong>Jenny has become increasingly concerned about the state of her city. She feels with the right information a platform to voice her concerns and connect with like minded peers. She has begun to surf the web is search of the right site.</p>
+	</body>
+</html>
