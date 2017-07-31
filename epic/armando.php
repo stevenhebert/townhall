7a9838cabcbedcff25b820284d6cb2189d369f5c
Michelle <mbojorquez4@cnm.edu>
2017-07-31 10:45:11 -0600

    commited prosona

--- epic/armando.php
+++ epic/armando.php
@@ -4,7 +4,7 @@
 		<title>Armando</title>
 	</head>
 	<body>
-		<h1>Person Armando</h1>Armando is a 24yr old native Albuqueran, he loves the city and has seen great changes over the past decade he has always been involed in his communnity by doing volunteer work.
+		<h1>Person Armando</h1>Armando is a 24yr old native Albuquerquean, he loves the city and has seen great changes over the past decade he has always been involed in his communnity by doing volunteer work.
 		<p><strong>Attitudes and Behaviors:</strong>Armando a potential user of the town hall because of his desire to know more about how local government works.  His main issue is he wants to be more involved in the decisions that are made for the city but dose'nt know how to engaged so his ideas are heard by the right people.</p>
 		<p><strong>Technology:</strong>Samsung - Galaxy J7 with 16GB Memory Microsoft - Surface Laptop – 13.5” - Intel Core i5 – 4GB Memory – 128GB Solid State Drive - user.</p>
 		<p><strong>Frustrations and Needs:</strong>Armando is frustrated because he has a hard time finding up todate information on local government and is unclear how local government works.</p>
