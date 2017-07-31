b0f01f9f89a7f2828ed7d62a58a87ea446044b12
steven hebert <shebert2@cnm.edu>
2017-07-31 02:18:55 -0600

    started executive summary

--- epic/executive-summary.php
+++ epic/executive-summary.php
@@ -6,6 +6,24 @@
 	</head>
 	<body>
 		<h1>Executive Summary</h1>
-		<p></p>
+		<p>Unique Selling Point</p>
+			ABQTownHall is the nexus of civic responsibilities and technology.
+
+		<p>Mission Statement</p>
+			To encourage participation and facilitate change by informing constituents and connecting them with their
+			representatives.
+
+		<p>Vision Statement</p>
+			Our vision is that ABQTownHall will serve as a non-partisan platform that empowers citizens, improve
+			representation, and creates a healthy democracy.
+
+		<p>Initiatives</p>
+
+		<p>Conclusions</p>
+
+		<p>Recommendations</p>
+
+		<p>Actions</p>
+
 	</body>
 </html>
