44aeb0cddf311b1aafa7019733472969ae91faf0
Michelle <mbojorquez4@cnm.edu>
2017-07-30 20:51:01 -0600

    commited prosona

--- epic/greta.php
+++ epic/greta.php
@@ -4,8 +4,11 @@
 		<title>Persona Named Greta</title>
 	</head>
 	<body>
-		<h1>Person named Greta</h1>
-		<p>This persona named Greta is a constituent and a potential user of the town hall.  Her main issue is that she's disabled and cannot attend regularly scheduled town halls because she's pretty much housebound.  She is very interested in politics but finds it hard to participate in the process because of her disability.  She is very interested in social services as well as services for the disabled.</p>
-
+		<h1>Person named Greta</h1> Greata is a 36yr old native Albuqueran, she loves the city and has seen great changes over the past decade she ahs always been involed in her communnity but just recently she has become disabled.
+		<p><strong>Attitudes and Behaviors:</strong>This persona named Greta is a constituent and a potential user of the town hall.  Her main issue is that she's disabled and cannot attend regularly scheduled town halls because she's pretty much housebound.  She is very interested in politics but finds it hard to participate in the process because of her disability.  She is very interested in social services as well as services for the disabled.</p>
+		<p><strong>Technology:</strong>Samsung - Galaxy J7 with 16GB Memory Microsoft - Surface Laptop – 13.5” - Intel Core i5 – 4GB Memory – 128GB Solid State Drive - user.</p>
+		<p><strong>Frustrations and Needs:</strong>Greta is frustrated because she disabled and cannot attend regularly scheduled town halls because she's pretty much housebound.  She is very interested in politics but finds it hard to participate in the process because of her disability
+		<p><strong>Goals:</strong>Greata wants to find a user frendly website to help linkher and inform her about city government.</p>
+		<p><strong>story</strong></p>Greata visits the socal media sites online but wishes all the information was condensed in a user friendly user website with the most uptoadte information
 	</body>
 </html>
