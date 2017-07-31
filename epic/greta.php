a4969abe26135957eaa6b822804498f015843e25
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-31 08:23:44 -0600

    added space for sections

--- epic/greta.php
+++ epic/greta.php
@@ -6,7 +6,15 @@
 		<title>Persona Named Greta</title>
 	</head>
 	<body>
-		<h1>Person named Greta</h1>
+		<h1>Persona named Greta</h1>
+		<p>Name</p>
+		<p>Age</p>
+		<p>Profession</p>
+		<p>Technology</p>
+		<p>Attitudes and Behaviors</p>
+		<p>Frustrations and Needs</p>
+		<p>Goals</p>
+		<p>User Story</p>
 		<p>This persona named Greta is a constituent and a potential user of the town hall.  Her main issue is that she's disabled and cannot attend regularly scheduled town halls because she's pretty much housebound.  She is very interested in politics but finds it hard to participate in the process because of her disability.  She is very interested in social services as well as services for the disabled.</p>
 
 	</body>
