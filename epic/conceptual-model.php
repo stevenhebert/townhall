9c4aaf0cdfe1ef9327af3892decff5d32ce61be7
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-08-01 13:48:24 -0600

    added persona information

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -6,6 +6,39 @@
 	</head>
 	<body>
 		<h1>Conceptual Model</h1>
-		<p></p>
+		<p>Profile</p>
+		<ul>
+			<li>profileId</li>
+			<li>profileDistrictId</li>
+			<li>profileFirstName</li>
+			<li>profileLastName</li>
+			<li>profileUserName</li>
+			<li>profileAddress1</li>
+			<li>profileAddress2</li>
+			<li>profileCity</li>
+			<li>profileState</li>
+			<li>profileZip</li>
+			<li>profileEmail</li>
+			<li>profileHash</li>
+			<li>profileSalt</li>
+		</ul>
+
+		<p>District</p>
+		<ul>
+			<li></li>
+
+		</ul>
+
+
+		<p>Post</p>
+		<ul>
+			<li></li>
+
+		</ul>
+		<p>Vote</p>
+		<ul>
+			<li></li>
+
+		</ul>
 	</body>
 </html>
