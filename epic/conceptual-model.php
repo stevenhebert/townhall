55303050f1b614e196ea7b3d33535429f1890dd4
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-08-01 15:40:42 -0600

    added persona information

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -8,24 +8,33 @@
 		<h1>Conceptual Model</h1>
 		<p>Profile</p>
 		<ul>
-			<li>profileId</li>
-			<li>profileDistrictId</li>
-			<li>profileFirstName</li>
-			<li>profileLastName</li>
-			<li>profileUserName</li>
-			<li>profileAddress1</li>
-			<li>profileAddress2</li>
-			<li>profileCity</li>
-			<li>profileState</li>
-			<li>profileZip</li>
-			<li>profileEmail</li>
-			<li>profileHash</li>
-			<li>profileSalt</li>
+			<li>profileId int autoincrement</li>
+			<li>profileDistrictId int</li>
+			<li>profileFirstName string</li>
+			<li>profileLastName string</li>
+			<li>profileUserName string</li>
+			<li>profileAddress1 string</li>
+			<li>profileAddress2 string</li>
+			<li>profileCity string</li>
+			<li>profileState string</li>
+			<li>profileZip string</li>
+			<li>profileEmail string</li>
+			<li>profileHash string</li>
+			<li>profileSalt string</li>
+			<li>profileActivationToken string</li>
+			<li>profileTypeId int</li>
+		</ul>
+
+		<p>type</p>
+		<ul><li>typeId int autoincrement</li>
+			<li>typeCode string</li>
 		</ul>
 
 		<p>District</p>
 		<ul>
-			<li></li>
+			<li>districtId int autoincrement</li>
+			<li>districtNumber string</li>
+			<li>districtGeom geometry</li>
 
 		</ul>
 
