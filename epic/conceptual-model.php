d9196d3d143eb66ab4ec512514e8d9af393d9d3c
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-08-01 16:08:10 -0600

    added vote and post tables

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -22,12 +22,7 @@
 			<li>profileHash string</li>
 			<li>profileSalt string</li>
 			<li>profileActivationToken string</li>
-			<li>profileTypeId int</li>
-		</ul>
-
-		<p>type</p>
-		<ul><li>typeId int autoincrement</li>
-			<li>typeCode string</li>
+			<li>profileRepresentative tinyint</li>
 		</ul>
 
 		<p>district</p>
@@ -41,12 +36,17 @@
 		<p>post</p>
 		<ul>
 			<li>postId int autoincrement</li>
-
+			<li>postProfileId int</li>
+			<li>postContent string</li>
+			<li>postDateTime datetime</li>
 		</ul>
+
 		<p>vote</p>
 		<ul>
-			<li></li>
-
+			<li>votePostId int</li>
+			<li>voteProfileId int</li>
+			<li>voteUpOrDown tinyint</li>
 		</ul>
+
 	</body>
 </html>
