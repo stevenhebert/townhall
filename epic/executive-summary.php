a1a70244a713b84681cd97869da4128b06172bbc
steven hebert <shebert2@cnm.edu>
2017-08-02 14:43:53 -0600

    formatted whitespace

--- epic/executive-summary.php
+++ epic/executive-summary.php
@@ -7,15 +7,15 @@
 	<body>
 		<h1>Executive Summary</h1>
 		<p>Unique Selling Point</p>
-			ABQTownHall is the nexus of civic responsibilities, technology, and local governance.
+		ABQTownHall is the nexus of civic responsibilities, technology, and local governance.
 
 		<p>Mission Statement</p>
-			To preserve democratic values and facilitate change by creating an informed, connected, and participating
-			constituent.
+		To preserve democratic values and facilitate change by creating an informed, connected, and participating
+		constituent.
 
 		<p>Vision Statement</p>
-			Our vision is for ABQTownHall to serve as a non-partisan platform that engages citizens, improves
-			representation, and helps maintain a healthy democracy.
+		Our vision is for ABQTownHall to serve as a non-partisan platform that engages citizens, improves
+		representation, and helps maintain a healthy democracy.
 
 		<p>Initiatives</p>
 
--- epic/greta.php
+++ epic/greta.php
@@ -7,14 +7,18 @@
 	</head>
 	<body>
 		<h1>Persona named Greta</h1>
-		<p>Name:  Greta M. Manners</p>
+		<p>Name: Greta M. Manners</p>
 		<p>Age: 65</p>
 		<p>Profession: Greta is retired</p>
 		<p>Technology: Windows PC Using Windows 7</p>
-		<p>Attitudes and Behaviors:  Greta is older and she is disabled.  She works from home and has a hard time leaving her house.  Because of this, she can't attend town hall meetings, and does not have any chance to get her questions answered.</p>
-		<p>Frustrations and Needs: Greta needs a way to get her questions answered.  She can't get to town hall meetings. Because she is housebound, it is impractical for her to attend meetings.</p>
+		<p>Attitudes and Behaviors: Greta is older and she is disabled. She works from home and has a hard time leaving
+			her house. Because of this, she can't attend town hall meetings, and does not have any chance to get her
+			questions answered.</p>
+		<p>Frustrations and Needs: Greta needs a way to get her questions answered. She can't get to town hall meetings.
+			Because she is housebound, it is impractical for her to attend meetings.</p>
 		<p>Goals: Greta needs a way to participate in the political process that she can do from her home.</p>
-		<p>User Story:  Greta wants to get her questions to her council representative and to the mayor.  The easiest thing for her to do would be to use her PC to submit questions.</p>
+		<p>User Story: Greta wants to get her questions to her council representative and to the mayor. The easiest thing
+			for her to do would be to use her PC to submit questions.</p>
 
 
 	</body>
