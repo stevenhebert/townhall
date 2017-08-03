d4a42f6a80b97c554b88a202445bc97256143ff6
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-08-03 15:58:03 -0600

    grammatical changes to armando

--- epic/armando.php
+++ epic/armando.php
@@ -13,21 +13,21 @@
 		<p><strong>Profession:</strong> Armando is Albuquerque's 2nd district representative. His district is
 			one of the most populated areas in the city and covers most of the downtown-commercial area.</p>
 
-		<p><strong>Technology:</strong> Armando uses a city issued blackberry for work related communications and
+		<p><strong>Technology:</strong> Armando uses a city-issued blackberry for work related communications and
 			a iPhone for personal communication. Likewise, his office is equipped with a Windows 10 machine but he uses a
 			Macbook in his leisure. Although limited in his choice of ISP's, Armando employs the lesser of the two evils
 			as his provider, Comcast.</p>
 
-		<p><strong>Attitudes and Behaviors:</strong> Armando is native Buquerquno with a green chile like passion for his
+		<p><strong>Attitudes and Behaviors:</strong> Armando is native Burqueno with a green-chile-like passion for his
 			hometown. This passion fuels his career choice as well as his work ethic. Although his credentials could land
 			him a higher government position, he prefers his council-ship as it allows him to work directly with his
 			constituents.</p>
 
 
 		<p><strong>Frustrations and Needs:</strong> Armando is constantly juggling requests from businesses and his
-			constituents, he has a PR analyst Gene who he uses sparingly to help with communications when he has too much
+			constituents. He has a PR analyst Gene who he uses sparingly to help with communications when he has too much
 			on his plate. Constrained by the city's dwindling budget, Armando is constantly searching for smarter solutions
-			to satisfy his districts requests.</p>
+			to satisfy his district's requests.</p>
 
 
 		<p><strong>Goals:</strong> Armando is always curious how his policies and statements are interpreted by his
@@ -37,8 +37,7 @@
 
 
 		<p><strong>User Story:</strong> One of the most arduous aspects of my job is determining how my constituents
-			interpret my policies. Some reveal this sentiment through social media but I find it can be a bit
-			biased. If only there were a forum all of them could access and leave their questions and criticisms for me to
-			respond.</p>
+			interpret my policies; some reveal this sentiment through social media but I find it can be a bit
+			biased. I need a forum all of them could access and leave their questions and criticisms for me.</p>
 	</body>
 </html>
