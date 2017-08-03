1af8ca17fc315084f64665f904d39385688ece36
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-08-03 15:31:43 -0600

    made changes to executive summary

--- epic/executive-summary.php
+++ epic/executive-summary.php
@@ -6,16 +6,10 @@
 	</head>
 	<body>
 		<h1>Executive Summary</h1>
-		<p>Unique Selling Point</p>
-		<p>Our application will give Albuquerque citizens a forum to submit questions and comments to their local representatives and allow the representatives to answer the questions.</p>
+		<p>ABQTownHall will give Albuquerque citizens a forum to submit questions and comments to their local representatives and allow the representatives to answer the questions. Our mission is to preserve democratic values and facilitate change by creating an informed, connected, and participating constituent. Our vision is for ABQTownHall to serve as a non-partisan platform that engages citizens, improves
+			representation, and helps maintain a healthy democracy.</p>
 
-		<p>Mission Statement</p>
-		ABQTownHall is the nexus of civic responsibilities, technology, and local governance.  Our mission is to preserve democratic values and facilitate change by creating an informed, connected, and participating
-		constituent.
 
-		<p>Vision Statement</p>
-		Our vision is for ABQTownHall to serve as a non-partisan platform that engages citizens, improves
-		representation, and helps maintain a healthy democracy.
 
 
 	</body>
