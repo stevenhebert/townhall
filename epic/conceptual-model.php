4f0f6935eb0bf42eca72bc72ab45215af3f84273
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-08-03 13:29:43 -0600

    cleaned up conceptual model

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -6,36 +6,38 @@
 	</head>
 	<body>
 		<h1>Conceptual Model</h1>
+		<h3>Entities</h3>
 		<p>profile</p>
 		<ul>
 			<li>profileId int autoincrement</li>
 			<li>profileDistrictId int</li>
-			<li>profileFirstName string</li>
-			<li>profileLastName string</li>
-			<li>profileUserName string</li>
+			<li>profileActivationToken string</li>
 			<li>profileAddress1 string</li>
 			<li>profileAddress2 string</li>
 			<li>profileCity string</li>
-			<li>profileState string</li>
-			<li>profileZip string</li>
 			<li>profileEmail string</li>
+			<li>profileFirstName string</li>
 			<li>profileHash string</li>
-			<li>profileSalt string</li>
-			<li>profileActivationToken string</li>
+			<li>profileLastName string</li>
 			<li>profileRepresentative tinyint</li>
+			<li>profileSalt string</li>
+			<li>profileState string</li>
+			<li>profileUserName string</li>
+			<li>profileZip string</li>
 		</ul>
 
 		<p>district</p>
 		<ul>
 			<li>districtId int autoincrement</li>
-			<li>districtNumber string</li>
 			<li>districtGeom geometry</li>
+			<li>districtName string</li>
 		</ul>
 
 
 		<p>post</p>
 		<ul>
 			<li>postId int autoincrement</li>
+			<li>postParentId int</li>
 			<li>postProfileId int</li>
 			<li>postContent string</li>
 			<li>postDateTime datetime</li>
@@ -45,20 +47,18 @@
 
 		<p>vote</p>
 		<ul>
-			<li>votePostId int</li>
+			<li>votePostId int autoincrement</li>
 			<li>voteProfileId int</li>
-			<li>voteUpOrDown tinyint</li>
+			<li>voteValue tinyint</li>
 		</ul>
 
-		<p>Relations</p>
+		<h3>Relations</h3>
 		<ul>
-			<li>One profile has one district, but one district is on many profile (1 to n)o</li>
-			<li>One post has one district, but one district can be on many posts (1 to n)</li>
+			<li>One profile has one district (1 to 1)</li>
+			<li>One post has one district (1 to 1)</li>
 			<li>Many profiles can vote on many posts (m to n)</li>
 			<li>Each post has one profile, but a profile can have many posts (1 to n)</li>
-			<li>One profile can have many votes, but only vote per post (1 to n)</li>
-			<li>One post can have many votes (1 to n)</li>
-		</ul>
+					</ul>
 
 	</body>
 </html>
