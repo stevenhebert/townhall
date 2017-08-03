efe7570752a1b1f14af5bb93431cd78586cd92c4
steven <shebert2@cnm.edu>
2017-08-03 00:21:35 -0600

    fixed formatting

--- epic/jenny.php
+++ epic/jenny.php
@@ -6,13 +6,37 @@
 	</head>
 	<body>
 		<h2>Persona</h2>
-		<p><strong>Name:</strong> Jenny Jenkins<p>
+		<p><strong>Name:</strong> Jenny Jenkins
+		<p>
 		<p><strong>Age:</strong> 24</p>
-		<p><strong>Technology:</strong> Mac and IPhone power user. Jenny uses a Mac Book Pro, and an IPhone 7 with an unlimited data plan. She uses Chrome browser because of it's cross-platform support and ease of use.</p>
-		<p><strong>Attitudes and Behaviors:</strong> Jenny is a college graduate and intellectually curious. She works in marketing and has a new found personal interest in politics and activism. She is actively looking for ways she can become more involved in local politics and connect with like minded individuals.</p>
-		<p><strong>Frustrations and Needs:</strong> Jenny is frustrated by how hard it to find information about city politics and link up with others for civil dialog. She needs a user friendly site that provides the information regarding city politics and a platform to discuss issues with her peers and maybe communicate directly with city politicians.</p>
-		<p><strong>Goals:</strong> Jenny wants to find a trustworthy site that clearly conveys the information about the city government she needs. She is comfortable with moderately complex websites that clearly state the relevant information.</p>
-		<p><strong>Story:</strong> Jenny has become increasingly concerned about the state of her city. Specially, the lack of safe parking downtown. Her vehicle was recently broken into. She feels with the right information a platform to voice her concerns and connect with like minded peers. She has begun to surf the web is search of the right site.</p>
-		<p><strong>Use Case:</strong> As she surfs the web, Jenny comes across the ABQ Town Hall website. She is intrigued by the mission statement and decides to click the sign-up button titled "we want you." She begins to fill in the relevant profile information including her first name, last name, username, address, and email. She clicks submit profile. Upon clicking submit, she gets a popup that says she has a email confirmation message on the way and that she will need to confirm. She checks her email and clicks on the hyperlink to confirm her email which redirects her to her personal landing page on the ABQ Town Hall. From her personal landing page she sees all of Albuquerque's nine City Council Districts and the Mayoral forums. As she clicks on each, she notices she has read-only access to all the forums, but she has write access to her City Council District and the Mayoral forum. To further drive home the point that she can only post in her City Council District and the Mayoral forum she receives a message stating this upon entering other City Council District forums. She decides to click on her City Council forum, then clicks create post, types a post, and submits. After posting, she decides to review a few other posts. As she reviews them she decides to up vote and down vote them based on she feels about the post.</p>
+		<p><strong>Technology:</strong> Mac and IPhone power user. Jenny uses a Mac Book Pro, and an IPhone 7 with an
+			unlimited data plan. She uses Chrome browser because of it's cross-platform support and ease of use.</p>
+		<p><strong>Attitudes and Behaviors:</strong> Jenny is a college graduate and intellectually curious. She works in
+			marketing and has a new found personal interest in politics and activism. She is actively looking for ways she
+			can become more involved in local politics and connect with like minded individuals.</p>
+		<p><strong>Frustrations and Needs:</strong> Jenny is frustrated by how hard it to find information about city
+			politics and link up with others for civil dialog. She needs a user friendly site that provides the information
+			regarding city politics and a platform to discuss issues with her peers and maybe communicate directly with
+			city politicians.</p>
+		<p><strong>Goals:</strong> Jenny wants to find a trustworthy site that clearly conveys the information about the
+			city government she needs. She is comfortable with moderately complex websites that clearly state the relevant
+			information.</p>
+		<p><strong>Story:</strong> Jenny has become increasingly concerned about the state of her city. Specially, the
+			lack of safe parking downtown. Her vehicle was recently broken into. She feels with the right information a
+			platform to voice her concerns and connect with like minded peers. She has begun to surf the web is search of
+			the right site.</p>
+		<p><strong>Use Case:</strong> As she surfs the web, Jenny comes across the ABQ Town Hall website. She is intrigued
+			by the mission statement and decides to click the sign-up button titled "we want you." She begins to fill in
+			the relevant profile information including her first name, last name, username, address, and email. She clicks
+			submit profile. Upon clicking submit, she gets a popup that says she has a email confirmation message on the
+			way and that she will need to confirm. She checks her email and clicks on the hyperlink to confirm her email
+			which redirects her to her personal landing page on the ABQ Town Hall. From her personal landing page she sees
+			all of Albuquerque's nine City Council Districts and the Mayoral forums. As she clicks on each, she notices she
+			has read-only access to all the forums, but she has write access to her City Council District and the Mayoral
+			forum. To further drive home the point that she can only post in her City Council District and the Mayoral
+			forum she receives a message stating this upon entering other City Council District forums. She decides to
+			click on her City Council forum, then clicks create post, types a post, and submits. After posting, she decides
+			to review a few other posts. As she reviews them she decides to up vote and down vote them based on she feels
+			about the post.</p>
 	</body>
 </html>
