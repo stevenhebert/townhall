ce38d7d4ad3e767c159127b4732a2e08b765872d
steven hebert <shebert2@cnm.edu>
2017-08-03 21:44:52 -0600

    attic'ed exe sum

---
+++ atic/executive-summary.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Executive Summary</title>
+	</head>
+	<body>
+		<h1>Executive Summary</h1>
+		<p>Unique Selling Point</p>
+		ABQTownHall is the nexus of civic responsibilities, technology, and local governance.
+
+		<p>Mission Statement</p>
+		To preserve democratic values and facilitate change by creating an informed, connected, and participating
+		constituent.
+
+		<p>Vision Statement</p>
+		Our vision is for ABQTownHall to serve as a non-partisan platform that engages citizens, improves
+		representation, and helps maintain a healthy democracy.
+
+		<p>Initiatives</p>
+
+		<p>Conclusions</p>
+
+		<p>Recommendations</p>
+
+		<p>Actions</p>
+
+	</body>
+</html>
--- epic/executive-summary.php
+++ epic/executive-summary.php
@@ -6,10 +6,11 @@
 	</head>
 	<body>
 		<h1>Executive Summary</h1>
-		<p>ABQTownHall will give Albuquerque citizens a forum to submit questions and comments to their local representatives and allow the representatives to answer the questions. Our mission is to preserve democratic values and facilitate change by creating an informed, connected, and participating constituent. Our vision is for ABQTownHall to serve as a non-partisan platform that engages citizens, improves
+		<p>ABQTownHall will give Albuquerque citizens a forum to submit questions and comments to their local
+			representatives and allow the representatives to answer the questions. Our mission is to preserve democratic
+			values and facilitate change by creating an informed, connected, and participating constituent. Our vision is
+			for ABQTownHall to serve as a non-partisan platform that engages citizens, improves
 			representation, and helps maintain a healthy democracy.</p>
-
-
 
 
 	</body>
