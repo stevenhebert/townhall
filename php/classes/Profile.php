7b8488abb5d48e4022d23da9dae01358233b285e
JRyanHenson <hensojr@gmail.com>
2017-08-08 15:24:27 -0600

    Updated Profile.php class file.
    https://app.asana.com/0/395328240769551/402889052603766

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -3,10 +3,11 @@
 namespace Edu\Cnm\Townhall;
 
 /**
- * Etsy data design project class for profile
+ * town hall capstone project class profile
  * @author Ryan Henson <hensojr@gmail.com>
  * @version 1.0.0
  **/
+
 class profile {
 	/**
 	 * id for this profile; this is the primary key
@@ -14,66 +15,121 @@ class profile {
 	 **/
 	private $profileId;
 	/**
-	 * activation toke for the profile
+	 * district id for this profile; this is a foreign key
+	 * @var int $profileDistrictId
+	 **/
+	private $profileDistrictId;
+	/**
+	 * activation token for the profile
 	 * @var string $profileActivationToken
 	 **/
 	private $profileActivationToken;
 	/**
-	 * handle associated with profile
-	 * @var string $profileAtHandle
+	 * first address field associated with profile
+	 * @var string $profileAddress1
 	 **/
-	private $profileAtHandle;
+	private $profileAddress1;
 	/**
-	 * email address associated with profile
-	 * @var string $profileEmail
+	 * second address field associated with profile
+	 * @var string $profileAddress2
 	 **/
+	private $profileAddress2;
+	/**
+	 * city associated with profile
+	 * @var string $profileCity
+	 **/
+	private $profileCity;
+	/**
+	 * email associated with profile
+	 * @var string $profileEmail
+	 */
 	private $profileEmail;
+	/**
+	 * first name associated with profile
+	 * @var string $profileFirstName
+	 */
+	private $profileFirstName;
 	/**
 	 * password hash associated with profile
 	 * @var string $profileHash
 	 **/
 	private $profileHash;
 	/**
-	 * phone number associated with profile
-	 * @var string $profilePhone
+	 * last name associated with profile
+	 * @var string $profileLastName
 	 **/
-	private $profilePhone;
+	private $profileLastName;
+	/**
+	 * whether or not profile is for representative
+	 * @var tinyint $profileRepresentative
+	 **/
+	private $profileRepresentative;
 	/**
 	 * salt used for password hash on associated profile
 	 * @var string $profileSalt
 	 **/
 	private $profileSalt;
-
+	/**
+	 * state associated with profile
+	 * @var string $profileState
+	 **/
+	private $profileState;
+	/**
+	 * username associated with profile
+	 * @var string $profileUserName
+	 **/
+	private $profileProfileUserName;
+	/**
+	 * zip associated with profile
+	 * @var string $profileZip
+	 **/
+	private $profileZip;
 	/**
 	 * constructor for this profile
 	 *
 	 * @param int|null $newProfileId of this profile or null if a new user
+	 * @param int $newProfileDistrictId of this profile
 	 * @param string $newProfileActivationToken of the user profile
-	 * @param string $newProfileAtHandle of the user profile
+	 * @param string $newProfileAddress1 of the user profile
+	 * @param string $newProfileAddress2 of the user profile
+	 * @param string $newProfileCity of the user profile
 	 * @param string $newProfileEmail of the user profile
+	 * @param string $newProfileFirstName of the user profile
 	 * @param string $newProfileHash of the user profile
-	 * @param string $newProfilePhone of the user profile
+	 * @param string $newProfileLastName of the user profile
+	 * @param int $newProfileRepresentative of the user profile
 	 * @param string $newProfileSalt of the user profile
+	 * @param string $newProfileState of the user profile
+	 * @param string $newProfileUserName of the user profile
+	 * @param string $newProfileZip of the user profile
 	 * @throws \InvalidArgumentException if data types are not valid
 	 * @throws \TypeError if data types violate type hints
 	 * @throws \Exception if some other exception occurs
 	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
 	 **/
-	public function __construct(?int $newProfileId, string $newProfileActivationToken, string $newProfileAtHandle, string $newProfileEmail, string $newProfileHash, string $newProfilePhone, string $newProfileSalt) {
+	public function __construct(?int $newProfileId, string $newProfileDistrictId, string $newProfileActivationToken, string $newProfileAddress1, string $newProfileAddress2, string $newProfileCity, string $newProfileEmail, string $newProfileFirstName, string $newProfileHash, string $newProfileLastName, int $newProfileRepresentative, string $newProfileSalt, string $newProfileState, string $newProfileUserName, string $newProfileZip) {
 		try {
 			$this->setProfileId($newProfileId);
+			$this->setProfileDistrictId($newProfileDistrictId);
 			$this->setProfileActivationToken($newProfileActivationToken);
-			$this->setProfileAtHandle($newProfileAtHandle);
+			$this->setProfileAddress1($newProfileAddress1);
+			$this->setProfileAddress2($newProfileAddress2);
+			$this->setProfileCity($newProfileCity);
 			$this->setProfileEmail($newProfileEmail);
+			$this->setProfileFirstName($newProfileFirstName);
 			$this->setProfileHash($newProfileHash);
-			$this->setProfilePhone($newProfilePhone);
+			$this->setProfileLastName($newProfileLastName);
+			$this->setProfileRepresentative($newProfileRepresentative);
 			$this->setProfileSalt($newProfileSalt);
+			$this->profileSalt($newProfileState);
+			$this->setProfileZip($newProfileZip);
 		} //determine what exception type was thrown
 		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
 	}
+
 	/**
 	 * accessor method for profileId
 	 *
@@ -105,6 +161,36 @@ class profile {
 	}
 
 	/**
+	 * accessor method for profileDistrictId
+	 *
+	 * @return int|null value of profileDistrictId
+	 **/
+	public function getProfileDistrictId(): int {
+		return ($this->profileDistrictId);
+	}
+
+	/**
+	 * mutator method for profileDistrictId
+	 *
+	 * @param int|null $newProfileDistrictId new value of profile district id
+	 * @throws \RangeException if $newProfileDistrictId is not positive
+	 * @throws \TypeError if $newProfileDistrictId is not an integer
+	 **/
+	public function setProfileDistrictId(?int $newProfileDistrictId): void {
+		//if product id is null immediately return it
+		if($newProfileDistrictId === null) {
+			$this->profileDistrictId = null;
+			return;
+		}
+		// verify the  profileId is positive
+		if($newProfileDistrictId <= 0) {
+			throw(new \RangeException("profile id is not positive"));
+		}
+		// convert and store the profileId
+		$this->profileDistrictId = $newProfileDistrictId;
+	}
+
+	/**
 	 * accessor method for profileActivationToken
 	 *
 	 * @return string value of profileActivationToken
@@ -112,6 +198,7 @@ class profile {
 	public function getProfileActivationToken(): string {
 		return ($this->profileActivationToken);
 	}
+
 	/**
 	 * mutator method for profileActivationToken
 	 *
@@ -133,34 +220,98 @@ class profile {
 		// store the profileActivationToken
 		$this->profileActivationToken = $newProfileActivationToken;
 	}
+
 	/**
-	 * accessor method for profileAtHandle
+	 * accessor method for profileAddress1
 	 *
-	 * @return string value of profifeAtHandle
+	 * @return string value of profileAddress1
 	 **/
-	public function getProfileAtHandle(): string {
-		return ($this->profileAtHandle);
+	public function getProfileAddress1(): string {
+		return ($this->profileAddress1);
 	}
+
 	/**
-	 * mutator method for profileAtHandle
+	 * mutator method for profileAddress1
 	 *
-	 * @param string $newProfileAtHandle new value of profileAtHandle
-	 * @throws \InvalidArgumentException if $newProfileAtHandle is not a string or insecure
-	 * @throws \TypeError if $newProfileAtHandle is not a string
+	 * @param string $newProfileAddress1 new value of profile
+	 * @throws \InvalidArgumentException if $newProfileAddress1 is not a string or insecure
+	 * @throws \TypeError if $newProfileAddress1 is not a string
 	 **/
-	public function setProfileAtHandle(string $newProfileAtHandle): void {
-		// verify the profileAtHandle is secure
-		$newProfileAtHandle = trim($newProfileAtHandle);
-		$newProfileAtHandle = filter_var($newProfileAtHandle, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		if(empty($newProfileAtHandle) === true) {
-			throw(new \InvalidArgumentException("profile at handle is empty or insecure"));
+	public function setProfileAddress1(string $newProfileAddress1): void {
+		// verify the profileAddress1 is secure
+		$newProfileAddress1 = trim($newProfileAddress1);
+		$newProfileAddress1 = filter_var($newProfileAddress1, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newProfileAddress1) === true) {
+			throw(new \InvalidArgumentException("profile address is empty or insecure"));
 		}
-		// verify the profileAtHandle will fit in the database
-		if(strlen($newProfileAtHandle) > 32) {
-			throw(new \RangeException("profile at table description content too large"));
+		// verify the profileAddress1 fit in the database
+		if(strlen($newProfileAddress1) > 64) {
+			throw(new \RangeException("profile address too large"));
 		}
-		// store the profileAtHandle
-		$this->profileActivationToken = $newProfileAtHandle;
+		// store the profileAddress1
+		$this->profileAddress1 = $newProfileAddress1;
+	}
+
+	/**
+	 * accessor method for profileAddress2
+	 *
+	 * @return string value of profileAddress2
+	 **/
+	public function getProfileAddress2(): string {
+		return ($this->profileAddress2);
+	}
+
+	/**
+	 * mutator method for profileAddress2
+	 *
+	 * @param string $newProfileAddress2 new value of profile
+	 * @throws \InvalidArgumentException if $newProfileAddress2 is not a string or insecure
+	 * @throws \TypeError if $newProfileAddress2 is not a string
+	 **/
+	public function setProfileAddress2(string $newProfileAddress2): void {
+		// verify the profileAddress1 is secure
+		$newProfileAddress2 = trim($newProfileAddress2);
+		$newProfileAddress2 = filter_var($newProfileAddress2, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newProfileAddress1) === true) {
+			throw(new \InvalidArgumentException("profile address is empty or insecure"));
+		}
+		// verify the profileAddress2 fit in the database
+		if(strlen($newProfileAddress2) > 64) {
+			throw(new \RangeException("profile address content too large"));
+		}
+		// store the profileAddress2
+		$this->profileAddress2 = $newProfileAddress2;
+	}
+
+	/**
+	 * accessor method for profileCity
+	 *
+	 * @return string value of profileCity
+	 **/
+	public function getProfileCity(): string {
+		return ($this->profileCity);
+	}
+
+	/**
+	 * mutator method for profileCity
+	 *
+	 * @param string $newProfileCity new value of profile
+	 * @throws \InvalidArgumentException if $newProfileCity is not a string or insecure
+	 * @throws \TypeError if $newProfileCity is not a string
+	 **/
+	public function setProfileCity(string $newProfileCity): void {
+		// verify the profileCity is secure
+		$newProfileCity = trim($newProfileCity);
+		$newProfileCity = filter_var($newProfileCity, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newProfileCity) === true) {
+			throw(new \InvalidArgumentException("profile city is empty or insecure"));
+		}
+		// verify the profileCity fit in the database
+		if(strlen($newProfileCity) > 64) {
+			throw(new \RangeException("profile city content too large"));
+		}
+		// store the profileCity
+		$this->profileCity = $newProfileCity;
 	}
 	/**
 	 * accessor method for profileEmail
@@ -170,6 +321,7 @@ class profile {
 	public function getProfileEmail(): string {
 		return ($this->profileEmail);
 	}
+
 	/**
 	 * mutator method for profileEmail
 	 *
@@ -191,6 +343,7 @@ class profile {
 		// store the profileEmail
 		$this->profileEmail = $newProfileEmail;
 	}
+
 	/**
 	 * accessor method for profileHash
 	 *
@@ -199,6 +352,7 @@ class profile {
 	public function getProfileHash(): string {
 		return ($this->profileHash);
 	}
+
 	/**
 	 * mutator method for profileHash
 	 *
@@ -220,35 +374,7 @@ class profile {
 		// store the profileHash
 		$this->profileHash = $newProfileHash;
 	}
-	/**
-	 * accessor method for profilePhone
-	 *
-	 * @return string value of profilePhone
-	 **/
-	public function getProfilePhone(): string {
-		return ($this->profilePhone);
-	}
-	/**
-	 * mutator method for profilePhone
-	 *
-	 * @param string $newProfilePhone new value of profilePhone
-	 * @throws \InvalidArgumentException if $newProfilePhone is not a string or insecure
-	 * @throws \TypeError if $newProfilePhone is not a string
-	 **/
-	public function setProfilePhone(string$newProfilePhone): void {
-		// verify the profilePhone is secure
-		$newProfilePhone = trim($newProfilePhone);
-		$newProfilePhone = filter_var($newProfilePhone, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		if(empty($newProfilePhone) === true) {
-			throw(new \InvalidArgumentException("profile phone is empty or insecure"));
-		}
-		// verify the profilePhone will fit in the database
-		if(strlen($newProfilePhone) > 32) {
-			throw(new \RangeException("profile phone content too large"));
-		}
-		// store the profilePhone
-		$this->profilePhone = $newProfilePhone;
-	}
+
 	/**
 	 * accessor method for profileSalt
 	 *
@@ -257,6 +383,7 @@ class profile {
 	public function getProfileSalt(): string {
 		return ($this->profileSalt);
 	}
+
 	/**
 	 * mutator method for profileSalt
 	 *
@@ -278,6 +405,38 @@ class profile {
 		// store the profileSalt
 		$this->profileSalt = $newProfileSalt;
 	}
+
+	/**
+	 * accessor method for profileUserName
+	 *
+	 * @return string value of profileUserName
+	 **/
+	public function getProfileUserName(): string {
+		return ($this->profileProfileUserName);
+	}
+
+	/**
+	 * mutator method for profileUserName
+	 *
+	 * @param string $newProfileUserName new value of profileUserName
+	 * @throws \InvalidArgumentException if $newProfileUserName is not a string or insecure
+	 * @throws \TypeError if $newProfileUserName is not a string
+	 **/
+	public function setProfileUserName(string $newProfileUserName): void {
+		// verify the profileUserName is secure
+		$newProfileUserName = trim($newProfileUserName);
+		$newProfileUserName = filter_var($newProfileUserName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newProfileUserName) === true) {
+			throw(new \InvalidArgumentException("profile at handle is empty or insecure"));
+		}
+		// verify the profileUserName will fit in the database
+		if(strlen($newProfileUserName) > 32) {
+			throw(new \RangeException("profile at table description content too large"));
+		}
+		// store the profileUserName
+		$this->profileUserName = $newProfileUserName;
+	}
+
 	/**
 	 * inserts this profile into mySQL
 	 *
@@ -302,6 +461,7 @@ class profile {
 		// update the null profileId with what mySQL just gave us
 		$this->profileId = intval($pdo->lastInsertId());
 	}
+
 	/**
 	 * deletes this profile from mySQL
 	 *
@@ -322,6 +482,7 @@ class profile {
 		$parameters = ["profileId" => $this->profileId];
 		$statement->execute($parameters);
 	}
+
 	/**
 	 * updates this profile in mySQL
 	 *
@@ -342,6 +503,7 @@ class profile {
 		$parameters = ["profileActivationToken" => $this->profileActivationToken, "profileAtHandle" => $this->profileAtHandle, "profileEmail" => $this->profileEmail, "profileHash" => $this->profileHash, "profilePhone" => $this->profilePhone, "profileSalt" => $this->profileSalt];
 		$statement->execute($parameters);
 	}
+
 	/**
 	 * gets the Profile by profileId
 	 *
@@ -379,4 +541,5 @@ class profile {
 		}
 		return($profile);
 	}
+
 }
