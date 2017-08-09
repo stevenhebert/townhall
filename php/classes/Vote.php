f3c72dd926b5bfa07cfa1b5892c5b4195c6b90a4
Michelle <mbojorquez4@cnm.edu>
2017-08-09 14:01:59 -0600

    made up dates to vote
    https://app.asana.com/0/395328240769551/402889052603768

--- php/classes/Vote.php
+++ php/classes/Vote.php
@@ -80,8 +80,8 @@ class Vote {
 			return;
 
 			// verify the postVote id is positive
-			if($newpostVoteId <= 0) {
-				throw(new \RangeException("postVoteId id is not positive"));
+			if($newPostVoteId <= 0) {
+				throw(new \RangeException("postVoteId is not positive"));
 			}
 
 			// convert and store the postVote id
@@ -93,8 +93,8 @@ class Vote {
 		 *
 		 * @return int value of vote profile id
 		 **/
-		public function getVoteProfileId() : int{
-			return($this->VoteProfileId);
+		public function getVoteProfileId() : int {
+			return ($this->VoteProfileId);
 		}
 
 		/**
@@ -104,6 +104,67 @@ class Vote {
 		 * @throws \RangeException if $newVoteProfileId is not positive
 		 * @throws \TypeError if $newVoteProfileId is not an integer
 		 **/
+		public function setVoteProfileId(int $newVoteProfileId) : void {
+
+			// verify the voteProfile id is positive
+			if($newVoteProfileId <= 0) {
+				throw(new \RangeException("vote profile id is not positive"));
+			}
+
+			// convert and store the vote profile id
+			$this->voteProfileId = $newVoteProfileId;
+		}
+		/**
+		 * accessor method for voteDateTime
+		 *
+		 * @return string value of tweet content
+		 **/
+		public function getVoteDateTime() : \DateTime {
+			return($this->VoteDateTime);
+		}
+		/**
+		 * mutator method for vote date/time
+		 *
+		 * @param \DateTime|string|null $newVoteDateTime vote date time as a DateTime object or string (or null to load the current time)
+		 * @throws \InvalidArgumentException if $newVoteDatetime is not a valid object or string
+		 * @throws \RangeException if $newVoteDateTime is a date that does not exist
+		 **/
+		public function setVoteDateTime($newVoteDateTime = null) : void {
+			// base case: if the date is null, use the current date and time
+			if($newVoteDateTime === null) {
+				$this->VoteDatetime = new \DateTime();
+				return;
+		}
+			// store the like date using the ValidateDate trait
+			try {
+				$newVoteDateTime = self::validateVoteDateTime($newVoteDateTime);
+			} catch(\InvalidArgumentException | \RangeException $exception) {
+				$exceptionType = get_class($exception);
+				throw(new $exceptionType($exception->getMessage(), 0, $exception));
+			}
+			$this->VoteDateTime = $newVoteDateTime;
+		}
+		/**
+		 * inserts this VoteDateTime into mySQL
+		 *
+		 * @param \PDO $pdo PDO connection object
+		 * @throws \PDOException when mySQL related errors occur
+		 * @throws \TypeError if $pdo is not a PDO connection object
+		 **/
+		public function insert(\PDO $pdo) : void {
+			// enforce the voteProfileId is null (i.e., don't insert a tweet that already exists)
+			if($this->voteProfileId !== null) {
+				throw(new \PDOException("not a new voteProfileId"));
+			}
+			// create query template
+			$query = "INSERT INTO vote(voteProfileId, voteContent, voteDate) VALUES(:voteProfileId, :voteContent, :voteDateTime)";
+			$statement = $pdo->prepare($query);
+			// bind the member variables to the place holders in the template
+			$formattedDate = $this->voteDate->format("Y-m-d H:i:s.u");
+			$parameters = ["votePostId" => $this->votePostId, "voteProfileId" => $this->voteProfileId, "voteDateTime" => $formattedVoteDateTime "voteValue"=> $this->voteValue];
+			$statement->execute($parameters);
+
+
 
 
 
