4fbd31746a6f2133cae1036d963125a7d082a242
Michelle <mbojorquez4@cnm.edu>
2017-08-09 09:44:17 -0600

    made up dates to vote
    https://app.asana.com/0/395328240769551/402889052603768

--- php/classes/Vote.php
+++ php/classes/Vote.php
@@ -37,8 +37,73 @@ class Vote {
 	 * @param int $newVoteProfileId id for the Profile that voted on this post
 	 * @param \DateTime|string|null $newVoteDateTime date and time vote was sent or null if set to current date and time
 	 * @param int $newVoteValue value for this vote
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 **/
+	public function __construct(?int $newPostVoteId, int $newVoteProfileId, $newVoteDateTime = null, int $newVoteValue) {
+		try {
+			$this->setPostVoteId($newPostVoteId);
+			$this->setVoteProfileId($newVoteProfileId);
+			$this->setVoteDateTime($newVoteDateTime);
+			$this->setVoteValue($newVoteValue);
+		}
+			//determine what exception type was thrown
+		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		}
+
+	/**
+	 * accessor method for postVoteId id
 	 *
+	 * @return int|null value of postVoteID id
+	 **/
+	public function getPostVoteId() : int {
+		return($this->postVoteId);
+	}
+
+/**
+ * mutator method for postVoteID id
+ *
+ * @param int|null $newPostVoteId new value of newPostVoteId id
+ * @throws \RangeException if $newPostVoteId is not positive
+ * @throws \TypeError if $newPostVoteId is not an integer
+ **/
+	public function setPostVoteId(?int $newPostVoteId) : void {
+		//if postVote id is null immediately return it
+		if($newPostVoteId === null) {
+			$this->postVoteId = null;
+			return;
+
+			// verify the postVote id is positive
+			if($newpostVoteId <= 0) {
+				throw(new \RangeException("postVoteId id is not positive"));
+			}
+
+			// convert and store the postVote id
+			$this->postVoteId = $newPostVoteId ;
+		}
+
+		/**
+		 * accessor method for vote profile id
+		 *
+		 * @return int value of vote profile id
+		 **/
+		public function getVoteProfileId() : int{
+			return($this->VoteProfileId);
+		}
+
+		/**
+		 * mutator method for vote profile id
+		 *
+		 * @param int $newVoteProfileId new value of vote profile id
+		 * @throws \RangeException if $newVoteProfileId is not positive
+		 * @throws \TypeError if $newVoteProfileId is not an integer
+		 **/
 
 
 
-}
