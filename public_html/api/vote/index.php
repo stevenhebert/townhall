ba8901a1ef281bef8d0fd960d3c009ce24547e1c
Michelle <mbojorquez4@cnm.edu>
2017-08-29 11:03:15 -0600

    updated activation
    https://app.asana.com/0/395328240769551/402889052603768

--- public_html/api/vote/index.php
+++ public_html/api/vote/index.php
@@ -6,7 +6,8 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 // inclusion of profile classe is strictly for testing purposes
-use Edu\Cnm\Townhall\Vote;
+use Edu\Cnm\Townhall\{District, Profile, Post, Vote};
+
 
 /**
  * api for the Vote class
