91704537e877d3fcfa944b2b027dc20429bceee2
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-08-29 11:25:30 -0600

    corrected error in signin
    https://app.asana.com/0/395328240769551/414028056380873

--- public_html/api/sign-in/index.php
+++ public_html/api/sign-in/index.php
@@ -2,7 +2,7 @@
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
-use Edu\Cnm\Townhall\Profile;
+use Edu\Cnm\Townhall\{Profile};
  /**
   * api for handling sign-in to townhall
   *
