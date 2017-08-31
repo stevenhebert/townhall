fda1dae4aba6d9d87c3e73b202258b5c1ad78c30
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-08-31 15:55:29 -0600

    changed head tag

--- static-ui/welcome.php
+++ static-ui/welcome.php
@@ -29,7 +29,7 @@
 
 				<!-- Latest compiled and minified JavaScript -->
 				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-				<title>Meow App 2</title>
+				<title>ABQ Town Hall</title>
 			</head>
 	</head>
 	<body>
