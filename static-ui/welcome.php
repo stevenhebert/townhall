7b6ab0b0088ab104b331ade7c3e4ab67a66f17b7
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-09-01 16:48:51 -0600

    created welcome page and added styles

--- static-ui/welcome.php
+++ static-ui/welcome.php
@@ -2,47 +2,73 @@
 		<html lang="en">
 			<head>
 				<meta charset="utf-8">
-				<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 				<meta name="viewport" content="width=device-width, initial-scale=1">
 
 				<!-- Latest compiled and minified CSS -->
 				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-				<!-- Google Fonts -->
-				<link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,300,700italic,700|Playfair+Display:400,400italic' rel='stylesheet' type='text/css'>
+				<!-- Optional theme -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 				<!-- Font Awesome -->
-				<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
+				<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+				<!-- custom css link -->
+				<link rel="stylesheet" type="text/css" href="lib/styles.css">
 
-				<!-- Custom CSS Goes HERE -->
-				<link rel="stylesheet" href="lib/style.css" type="text/css">
+
+
+				<!--font links -->
+
+
+				<link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+
+
 
 				<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-				<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 				<!--[if lt IE 9]>
-				<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-				<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+				<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+				<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 				<![endif]-->
 
-				<!-- jQuery - required for Bootstrap Components -->
+				<!-- jquery v3.0 -->
 				<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
 				<!-- Latest compiled and minified JavaScript -->
 				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+				<!-- jQuery -->
+				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+				<!-- jQuery Form, Additional Methods, Validate -->
+				<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+				<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+				<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+				<!-- Your JavaScript Form Validator -->
+				<script src="js/form-validate.js"></script>
+
+				<!-- Google reCAPTCHA -->
+				<script src='https://www.google.com/recaptcha/api.js'></script>
+
+
 				<title>ABQ Town Hall</title>
 			</head>
 	</head>
 	<body>
+		<div class="container">
 
 		<h1>Welcome to Albuquerque Virtual Town Hall</h1>
 		<div class="row">
-			<div class="col-6-sm">
+			<div class="col-6-sm offset4">
+				<form class="form-container">
 				<h4>Login</h4>
 				<label>Email</label>
 				<input type="text" name="email"/>
 				<label>Password</label>
 				<input type="text" name="password"/>
 				<button class="btn btn-success">Login</button>
+				</form>
 
 			</div><!--column-->
 			<div class="col-sm-6">
@@ -50,5 +76,6 @@
 
 			</div><!--column-->
 		</div><!--row-->
+		</div><!--container-->
 	</body>
 </html>
