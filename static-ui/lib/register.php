ab270241d9a4a183705fc8f6d70c34803eeb561c
Michelle <mbojorquez4@cnm.edu>
2017-08-31 22:28:44 -0600

    finished registation form
    https://app.asana.com/0/395328240769551/402889052603768

--- static-ui/lib/register.php
+++ static-ui/lib/register.php
@@ -1,80 +1,100 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-        <!-- Google Fonts -->
-        <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,300,700italic,700|Playfair+Display:400,400italic' rel='stylesheet' type='text/css'>
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,300,700italic,700|Playfair+Display:400,400italic' rel='stylesheet' type='text/css'>
 
-        <!-- Font Awesome -->
-        <link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
+    <!-- Font Awesome -->
+    <link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
 
-        <!-- Custom CSS Goes HERE -->
-        <link rel="stylesheet" href="css/style.css" type="text/css">
+    <!-- Custom CSS Goes HERE -->
+    <link rel="stylesheet" href="css/style.css" type="text/css">
 
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-        <!-- jQuery - required for Bootstrap Components -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+    <!-- jQuery - required for Bootstrap Components -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <title>Townhall</title>
-    </head>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <title>Registration</title>
+</head>
 <body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6"</div>
+        <div class="col-md-6"></div>
+    </div>
+
+<h1>Sign up</h1>
+<form>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="First Name" type="text" class="form-control" name="First Name" placeholder="First Name">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Last Name" type="text" class="form-control" name="Last Name" placeholder="Last Name">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Address" type="text" class="form-control" name="Address" placeholder="Address">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Address2" type="text" class="form-control" name="Address2" placeholder="Address2">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="City" type="text" class="form-control" name="msg" placeholder="City">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="State" type="text" class="form-control" name="State" placeholder="State">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Zip Code" type="text" class="form-control" name="Zip Code" placeholder="Zip Code">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Email" type="text" class="form-control" name="Email" placeholder="Email">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+    </div>
+
+</form>
+<h1>Sign in</h1>
+<form>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="User Name" type="text" class="form-control" name="User Name" placeholder="User Name">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Password" type="text" class="form-control" name="Password" placeholder="Password">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Re Enter Password" type="text" class="form-control" name="Re Enter Password" placeholder="Re Enter Password">
+    </div>
     <div class="container">
-    <h1>Registration</h1>
-    <p></p>
-    <form>
-        <div class="form-group">
-            <label for="inputsm">First name*</label>
-            <input class="form-control input-sm" id="inputsm" type="text">
-        </div>
-        <div class="form-group">
-            <label for="inputsm">Last name*</label>
-            <input class="form-control input-sm" id="inputsm" type="text">
-        </div>
-        <div class="form-group">
-            <label for="inputsm">Address 1*</label>
-            <input class="form-control input-sm" id="inputsm" type="text">
-        </div>
-        <div class="form-group">
-            <label for="inputsm">Address 2</label>
-            <input class="form-control input-sm" id="inputsm" type="text">
-        </div>
-        <div class="form-group">
-            <label for="inputsm">City</label>
-            <select class="form-control input-sm" id="sel2">
-                <option>Albuquerque</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="sel3">zip</label>
-            <select class="form-control input-sm" id="sel3">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-            </select>
-        </div>
-    </form>
-</div>
+        <button type="button" class="btn btn-info btn-lg">Submit</button>
+    </div>
 
-</body>
-</html>
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
