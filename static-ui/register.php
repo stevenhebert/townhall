9afdfbdb12d074766c6d46fc0d8fa7000b6d7dc5
Michelle <mbojorquez4@cnm.edu>
2017-09-02 15:26:18 -0600

    finished registation form
    https://app.asana.com/0/395328240769551/402889052603768

---
+++ static-ui/register.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Font Awesome -->
+        <link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+        <!-- custom css link -->
+        <link rel="stylesheet" type="text/css" href="lib/styles.css">
+
+
+
+        <!--font links -->
+
+
+        <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+
+
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- jquery v3.0 -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+        <!-- jQuery Form, Additional Methods, Validate -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+        <!-- Your JavaScript Form Validator -->
+        <script src="js/form-validate.js"></script>
+
+        <!-- Google reCAPTCHA -->
+        <script src='https://www.google.com/recaptcha/api.js'></script>
+
+
+
+        <title>ABQ Town Hall</title>
+    </head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6"</div>
+        <div class="col-md-6"></div>
+    </div>
+
+<h1>Sign up</h1>
+<form>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="First Name" type="text" class="form-control" name="First Name" placeholder="First Name">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Last Name" type="text" class="form-control" name="Last Name" placeholder="Last Name">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Address" type="text" class="form-control" name="Address" placeholder="Address">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Address2" type="text" class="form-control" name="Address2" placeholder="Address2">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="City" type="text" class="form-control" name="msg" placeholder="City">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="State" type="text" class="form-control" name="State" placeholder="State">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Zip Code" type="text" class="form-control" name="Zip Code" placeholder="Zip Code">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Email" type="text" class="form-control" name="Email" placeholder="Email">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+    </div>
+    <div class="input-group">
+        <span class="input-group-addon"></span>
+        <input id="Re Enter Password" type="text" class="form-control" name="Re Enter Password" placeholder="Re Enter Password">
+    </div>
+    <div class="container">
+        <button type="button" class="btn btn-info btn-lg">Submit</button>
+    </div>
+
+</body>
+</html>
