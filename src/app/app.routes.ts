09235872f4a7fa22ce1661a6412d19f1d95f2192
JRyanHenson <hensojr@gmail.com>
2017-09-03 13:09:43 -0600

    Updated app.routes.ts with project relevant info.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,9 @@
 import {RouterModule, Routes} from "@angular/router";
-// import {BaconComponent} from "./components/bacon-component";
 import {HomeComponent} from "./components/home-component";
-// import {BaconService} from "./services/bacon-service";
 
 export const allAppComponents = [HomeComponent];
 
 export const routes: Routes = [
-	// {path: "bacon", component: BaconComponent},
 	{path: "", component: HomeComponent}
 ];
 
