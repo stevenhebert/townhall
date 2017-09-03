55d43626ce46202d016e7e5ab162066a90a52c41
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-09-03 10:17:22 -0600

    update to html content

--- static-ui/posts.php
+++ static-ui/posts.php
@@ -115,7 +115,7 @@
 					</div>
 
 					<div class="col-md-8">
-						<h1>Posting in District Number </h1>
+						<h1>Posts in District Number </h1>
 
 						<!-- Begin Post Item -->
 						<div class="panel panel-default">
