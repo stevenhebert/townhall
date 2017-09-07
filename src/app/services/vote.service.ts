0bbe7db72c623dd136ae8d718a3a70238cf70e20
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-09-07 10:19:12 -0600

    completed vote service

---
+++ src/app/services/vote.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {BaseService} from "./base.service";
+import {Vote} from "../classes/vote";
+import {Status} from "../classes/status";
+
+@Injectable()
+export class PostService extends BaseService {
+	constructor(protected http: Http) {
+		super(http);
+	}
+
+	private voteUrl = "api/vote/";
+	/* get vote by composite key
+	* create new vote
+	* update vote value
+
+	 */
+
+	getAllVotes() : Observable<Vote[]> {
+		return(this.http.get(this.voteUrl)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getVoteByPostIdAndProfileId(votePostId : number, voteProfileId : number) : Observable<Vote> {
+		return(this.http.get(this.voteUrl + votePostId + voteProfileId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	createVote(vote : Vote) : Observable<Status> {
+		return(this.http.post(this.voteUrl, vote)
+			.map(this.extractMessage)
+			.catch(this.handleError));
+	}
+
+	editVote(vote: Vote) : Observable<Status> {
+		return(this.http.put(this.voteUrl + vote.votePostId + vote.voteProfileId, vote)
+			.map(this.extractMessage)
+			.catch(this.handleError));
+	}
+}
