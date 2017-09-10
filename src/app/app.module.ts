73e2b1c806d04ebdc706f1669cf8c22ef0588b69
JRyanHenson <hensojr@gmail.com>
2017-09-10 13:02:32 -0600

    Updated app.routes.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {SignInService} from "./services/signin.service";
 // import {SignOutService} from "./services/signout.service";
 import {SignUpService} from "./services/signup.service";
 import {SessionService} from "./services/session.service";
+import {CookieService} from "ng2-cookies";
 
 const moduleDeclarations = [AppComponent];
 
@@ -22,8 +23,20 @@ const moduleDeclarations = [AppComponent];
 		appRoutingProviders,
 		SignUpService,
 		SignInService,
-		SessionService
+		SessionService,
+		CookieService
 	]
 })
 export class AppModule {
+	cookieJar : any = {};
+
+	constructor(protected cookieService: CookieService, protected sessionService: SessionService) {}
+
+	run() : void {
+				this.sessionService.setSession()
+					.subscribe(response => {
+							this.cookieJar = this.cookieService.getAll();
+							console.log(this.cookieJar);
+						});
+			}
 }
