e37dd272f0f1f9eae33ed946256ce1656386730e
steven hebert <hebertsteven@me.com>
2017-11-05 03:09:03 -0700

    Update todo.php

--- todo.php
+++ todo.php
@@ -2,50 +2,37 @@ TODO:
 
 
 
-NOW:
+NOW: 
 
 Forgot password / password reset
-
+Limit activation token life span (24 hour expire)
 Resend activation
 
-Redirect after authorization token activation broken
-
-Limit activation token life span (24 hour expire)
-
 Show district number even if there are no posts
+Header with information & D3 map
 
-Mail@abqtownhall is getting marked as spam
 
+
+SOON:
+
+Prevent mail@abqtownhall from getting marked as spam
 Prioritize posts by vote / determine best method to sort
-
 Wire up contact form
-
 Change state from a text field to a drop down so that only state abbreviations can be entered (sign-up & edit profile)
-
 Show number of replies to parentPosts in parent header
-
 Add flare for officials accounts
-
+Port to mobile with ionic by January 4, 2018
 
 
 
 LATER:
 
 Check error messages for end-user-ability
-
 Search engine optimization
-
 Get-text for language translation
-
 Notify by email if password is changed an allow user to deny change if unrecognized
-
 Basic password rules (block common passwords, min length, etc)
-
 Send user email when password is changed
-
 Issue new session when address is changed -> otherwise they still only have access to their old district and not new
-
 Only allow user to change address once per day so they can just keep changing address to troll other districts
-
-
-interface email token, decrement with for loop
+Increase coverage
