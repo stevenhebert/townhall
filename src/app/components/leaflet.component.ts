75f8e12076e6aaf1c8648fd9d3ee509e381a3a32
steven hebert <hebertsteven@me.com>
2017-12-05 23:39:51 -0700

    added basic map functionality via leaflet

--- src/app/components/leaflet.component.ts
+++ src/app/components/leaflet.component.ts
@@ -20,8 +20,3 @@ export class LeafletComponent {
 
 
 }
-layers = [
-	circle([ 46.95, -122 ], { radius: 5000 }),
-	polygon([[ 46.8, -121.85 ], [ 46.92, -121.92 ], [ 46.87, -121.8 ]]),
-	marker([ 46.879966, -121.726909 ])
-];
