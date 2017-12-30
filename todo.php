12b3a9123a5e79f555b4ee92c37b30eb2078598e
steven hebert <hebertsteven@me.com>
2017-12-30 01:20:16 -0600

    font sizes reduced to fix mobile ui issues

--- todo.php
+++ todo.php
@@ -4,24 +4,21 @@ TODO:
 
 NOW: 
 
-Forgot password expire
-Limit activation token life span (24 hour expire)
 Resend activation
 Show district number even if there are no posts
-Header with information & D3 map
-Recovery reset on Init
+Prioritize posts by vote / determine best method to sort
+Port to mobile with ionic by January 4, 2018
+Add flare for officials accounts
 
 
 
 SOON:
 
 Prevent mail@abqtownhall from getting marked as spam
-Prioritize posts by vote / determine best method to sort
+Limit activation token life span (24 hour expire)
 Wire up contact form
 Change state from a text field to a drop down so that only state abbreviations can be entered (sign-up & edit profile)
 Show number of replies to parentPosts in parent header
-Add flare for officials accounts
-Port to mobile with ionic by January 4, 2018
 
 
 
@@ -31,7 +28,5 @@ Search engine optimization
 Get-text for language translation
 Notify by email if password is changed and allow user to deny change if unrecognized
 Basic password rules (block common passwords, min length, etc)
-Send user email when password is changed
 Issue new session when address is changed -> otherwise they still only have access to their old district and not new
 Only allow user to change address once per day so they can just keep changing address to troll other districts
-Gentelella
